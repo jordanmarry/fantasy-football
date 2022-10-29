21c500d537d8fd699d88ca32d868d6ad2e939b07
Jordan <jmarry@terpmail.umd.edu>
2022-10-29 16:15:20 -0400

    merged the main activity and login activity

--- app/src/main/java/com/example/fantasyfootball/MainFragment.kt
+++ app/src/main/java/com/example/fantasyfootball/MainFragment.kt
@@ -1,24 +1,30 @@
 package com.example.fantasyfootball
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.fantasyfootball.databinding.MainFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainFragment: Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    /** Binding to XML layout */
+    private lateinit var binding: MainFragmentBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Use the provided ViewBinding class to inflate the layout.
-        val binding = MainFragmentBinding.inflate(inflater, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
 
-        binding.mainLogin.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_mainFragment_to_loginFragment
-            )
-        }
+        firebaseAuth = requireNotNull(FirebaseAuth.getInstance())
+
+        binding.loginLogin.setOnClickListener { loginUserAccount() }
 
         binding.register.setOnClickListener {
             findNavController().navigate(
@@ -28,4 +34,51 @@ class MainFragment: Fragment() {
 
         return binding.root
     }
+
+    private fun loginUserAccount() {
+        val email: String = binding.loginEmail.text.toString()
+        val password: String = binding.loginPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.login_toast),
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.password_toast),
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
+        binding.loginProgressBar.visibility = View.VISIBLE
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                binding.loginProgressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Login successful!",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    findNavController().navigate(
+                        R.id.action_mainFragment_to_dashboardFragment
+                    )
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Login failed. Please try again later",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 }
