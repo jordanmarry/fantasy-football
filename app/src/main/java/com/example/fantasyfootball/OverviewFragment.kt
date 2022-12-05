b0d6ac711633d9eacd2b51a5f981b087859f74f4
Jordan <jmarry@terpmail.umd.edu>
2022-12-05 04:00:19 -0500

    overview almost done. Advice is done. Testing still needs to be done

--- app/src/main/java/com/example/fantasyfootball/OverviewFragment.kt
+++ app/src/main/java/com/example/fantasyfootball/OverviewFragment.kt
@@ -1,17 +1,24 @@
 package com.example.fantasyfootball
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.fantasyfootball.databinding.ActivityAdviceBinding
 import com.example.fantasyfootball.databinding.OverviewFragmentBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
 
 class OverviewFragment : Fragment(), PlayerClickListener {
     private lateinit var database: DatabaseReference
+    private lateinit var dbref: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding : OverviewFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,90 +26,134 @@ class OverviewFragment : Fragment(), PlayerClickListener {
         savedInstanceState: Bundle?
     ): View {
         // creating the binding root
-        val binding = OverviewFragmentBinding.inflate(inflater, container, false)
+        binding = OverviewFragmentBinding.inflate(inflater, container, false)
+
+        getLeagueData()
 
         return binding.root
     }
 
+    private fun getLeagueData(){
+        auth = requireNotNull(FirebaseAuth.getInstance())
+        val email = auth.currentUser?.email
+        val key = email?.substring(0, email.indexOf('@'))
+        dbref = FirebaseDatabase.getInstance().getReference("users/$key/leagues")
 
-    fun rosterSharePercentage()  {
+        dbref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    val arr = arrayListOf<League>()
+                    for (i in snapshot.children){
+                        val p = i.getValue(League::class.java)!!
+                        arr.add(p)
+                    }
+                    rosterSharePercentage(arr)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+
+    @SuppressLint("UseRequireInsteadOfGet")
+    fun rosterSharePercentage(leagueList: ArrayList<League>)  {
         // find first empty team
         // database = FirebaseDatabase.getInstance().getReference("users")
 
         // get the current user and check to see their first empty team slot
         // once found, go through playersIn and find each player in the players database
         // then add the player obj to playerList, then add playerList to the first empty team
-        val auth = requireNotNull(FirebaseAuth.getInstance())
-        val email = auth.currentUser?.email
-
-        val key = email?.substring(0, email.indexOf('@'))
 
         val mostOwned = HashMap<Player, Int>()
         val allTeams = HashMap<String, Int>()
+        var allTeamsCopy = HashMap<String, Int>()
         val bestPlayers = HashMap<String, Player>()
 
-        // probs need to change wont lie
-        val leagueList = database.child("users").child(key!!).child("leagues") as ArrayList<League>
+        // Getting the Data primed
+        for (l in leagueList){
+            for (p in l.playerList!!){
+                mostOwned[p] = 0
+                allTeams[p.team!!] = 0
 
-        for(l in leagueList){
-            for(p in l.playerList!!){
+                if (!bestPlayers.containsKey(p.pos)){
+                    bestPlayers.put(p.pos!!, p)
+                } else if(bestPlayers.containsKey(p.pos) && bestPlayers.get(p.pos)!!.ffPoints!! < p.ffPoints!!){
+                    bestPlayers.put(p.pos!!, p)
+                }
+            }
+        }
+
+        for (l in leagueList) {
+            for (p in l.playerList!!) {
                 var count = mostOwned.get(p) as Int
                 count += 1
                 mostOwned.put(p, count)
                 var teamCount = allTeams.get(p.team) as Int
                 teamCount += 1
                 allTeams.put(p.team!!, teamCount)
-                if(bestPlayers.containsKey(p.pos) && bestPlayers.get(p.pos)!!.ffPoints!! < p.ffPoints!!){
-                    bestPlayers.put(p.pos!!, p)
-                } else if (!bestPlayers.containsKey(p.pos)){
-                    bestPlayers.put(p.pos!!, p)
-                }
             }
         }
+
+        var full = 0
+
+        for(i in allTeams.keys){
+            allTeamsCopy[i] = allTeams.get(i) as Int
+        }
+
+        for (i in allTeams.keys){
+            full += allTeams[i]!!
+        }
+
         var fiveMostOwned = ArrayList<Player>()
         var threeMostTeams = ArrayList<String>()
 
-        var mostOwnedPlayer = findMostOwned(mostOwned)
-        fiveMostOwned.add(mostOwnedPlayer)
-        mostOwned.remove(mostOwnedPlayer)
+        while (mostOwned.size != 0 && fiveMostOwned.size < 5){
+            var mostOwnedPlayer = findMostOwned(mostOwned)
+            fiveMostOwned.add(mostOwnedPlayer)
+            mostOwned.remove(mostOwnedPlayer)
+        }
 
-        mostOwnedPlayer = findMostOwned(mostOwned)
-        fiveMostOwned.add(mostOwnedPlayer)
-        mostOwned.remove(mostOwnedPlayer)
+        while (allTeams.size != 0 && threeMostTeams.size < 3){
+            var mostOwnedTeam = findMostTeams(allTeams)
+            threeMostTeams.add(mostOwnedTeam)
+            allTeams.remove(mostOwnedTeam )
+        }
 
-        mostOwnedPlayer = findMostOwned(mostOwned)
-        fiveMostOwned.add(mostOwnedPlayer)
-        mostOwned.remove(mostOwnedPlayer)
+        val threeMostTeamsMap = HashMap<String, Double>()
 
-        mostOwnedPlayer = findMostOwned(mostOwned)
-        fiveMostOwned.add(mostOwnedPlayer)
-        mostOwned.remove(mostOwnedPlayer)
+        for (i in threeMostTeams){
+            threeMostTeamsMap[i] = ((allTeamsCopy[i]!!.toDouble()/full.toDouble())*100.0)
+        }
+        // Potentially Make a Team Card. Team Card Class. Team Adapter Class. Team XML.
 
-        mostOwnedPlayer = findMostOwned(mostOwned)
-        fiveMostOwned.add(mostOwnedPlayer)
-        mostOwned.remove(mostOwnedPlayer)
-
-        var mostOwnedTeam = findMostTeams(allTeams)
-        threeMostTeams.add(mostOwnedTeam )
-        allTeams.remove(mostOwnedTeam )
-
-        mostOwnedTeam  = findMostTeams(allTeams)
-        threeMostTeams.add(mostOwnedTeam )
-        allTeams.remove(mostOwnedTeam )
-
-        mostOwnedTeam  = findMostTeams(allTeams)
-        threeMostTeams.add(mostOwnedTeam )
-        allTeams.remove(mostOwnedTeam )
 
         // fiveMostOwned contains five most owned player objects across leagues
         // threeMostTeams contains three most owned teams across leagues
         // bestPlayers map that contains best player at each position
+        val mostRecyclerView = binding.mostRecyclerView
+        mostRecyclerView.layoutManager = GridLayoutManager(activity,1)
+        mostRecyclerView.adapter = PlayerAdapter(fiveMostOwned, this@OverviewFragment, this@OverviewFragment.activity!!.applicationContext)
+
+        val bestPlayersArr = arrayListOf<Player>()
+        for (i in bestPlayers.keys) {
+            bestPlayersArr.add(bestPlayers[i]!!)
+        }
+
+        Log.d("HERE", bestPlayersArr.toString())
+
+        val topPosRecyclerView = binding.topPosRecyclerView
+        topPosRecyclerView.layoutManager = GridLayoutManager(activity,1)
+        topPosRecyclerView.adapter = PlayerAdapter(bestPlayersArr, this@OverviewFragment, this@OverviewFragment.activity!!.applicationContext)
 
 
     }
 
-    private fun findMostOwned(mostOwned: HashMap<Player, Int>): Player {
-        var best: Player = mostOwned.get(mostOwned.keys.first()) as Player
+    private fun findMostOwned(mostOwned: HashMap<Player, Int>) : Player {
+        var best: Player = mostOwned.keys.iterator().next()
         var max = 0
         for(k in mostOwned.keys){
             if(mostOwned[k]!! > max){
